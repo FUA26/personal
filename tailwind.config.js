5ad501676578f9ea7a19e07cd075ee751b7529a3
Fatih <Fatihulil@gmail.com>
2023-06-16 15:20:06 +0700

    feat: :lipstick: setup tailwind support dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +8,34 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'fuaprimary': {
+        100: '#CAE7FC',
+        200: '#96CCFA',
+        300: '#60A9F1',
+        400: '#3987E4',
+        500: '#0058D3',
+        600: '#0044B5',
+        700: '#003297',
+        800: '#00237A',
+        900: '#001965',
+      },
+      'fuasecondary': {
+        100: '#D8FCE8',
+        200: '#B3FAD9',
+        300: '#8AF0CB',
+        400: '#69E2C2',
+        500: '#3CD0B6',
+        600: '#2BB2A7',
+        700: '#1E9495',
+        800: '#136D78',
+        900: '#0B5263',
+      },
+      // ...
+    },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -14,5 +43,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  
 }
