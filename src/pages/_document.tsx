6f28f5b2ec03a327fd1e61b355038563a1e4c75d
Fatih <Fatihulil@gmail.com>
2023-06-18 00:09:35 +0700

    fix: :mag: fixing head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,22 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 import { Inter } from 'next/font/google';
+const defaultMeta = {
+  title: 'Fatih U A',
+  siteName: 'Fatihua.com',
+  description: 'Fatih Ulil Albab personal website ',
+  url: 'https://fatihua.com',
+  image: 'https://fatihua.com/favicon/favicon-32x32.png',
+  type: 'website',
+};
 
 export default function Document() {
-  
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>{defaultMeta.title}</title>
+        <meta content={defaultMeta.description} name="description" />
+        <meta property="og:url" content={`${defaultMeta.url}`} />
+      </Head>
       <body>
         <Main />
         <NextScript />
